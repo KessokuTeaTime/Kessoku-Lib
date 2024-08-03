60b008c27bff6d62fa334ffcfe887d86f5a76a15
RawDiamondMC <RawDiamondMC@outlook.com>
2024-08-03 20:25:10 +0800

    more api for platform

--- platform-neo/src/main/java/band/kessoku/lib/platform/impl/ModLoaderImpl.java
+++ platform-neo/src/main/java/band/kessoku/lib/platform/impl/ModLoaderImpl.java
@@ -4,8 +4,12 @@ import band.kessoku.lib.platform.api.Env;
 import band.kessoku.lib.platform.api.ModData;
 import band.kessoku.lib.platform.api.ModLoader;
 import com.google.auto.service.AutoService;
+import net.minecraft.MinecraftVersion;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.fml.loading.FMLPaths;
 
+import java.nio.file.Path;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -31,5 +35,25 @@ public class ModLoaderImpl implements ModLoader {
     @Override
     public Env getEnv() {
         return FMLLoader.getDist().isClient() ? Env.CLIENT : Env.SERVER;
+    }
+
+    @Override
+    public Path getGameFolder() {
+        return FMLPaths.GAMEDIR.get();
+    }
+
+    @Override
+    public Path getConfigFolder() {
+        return FMLPaths.CONFIGDIR.get();
+    }
+
+    @Override
+    public Path getModsFolder() {
+        return FMLPaths.MODSDIR.get();
+    }
+
+    @Override
+    public boolean isModLoaded(String id) {
+        return ModList.get().isLoaded(id);
     }
 }
