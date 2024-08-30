d58af10c99f719e3dad71e447d26818977e1f8fa
Oganesson897 <101081378+Oganesson897@users.noreply.github.com>
2024-08-31 08:18:40 +0900

    entity events base

---
+++ entity-events/common/src/main/java/band/kessoku/lib/events/entity/KessokuEntityEvents.java
@@ -0,0 +1,9 @@
+package band.kessoku.lib.events.entity;
+
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+public class KessokuEntityEvents {
+    public static final String MOD_ID = "kessoku_entity_events";
+    public static final Marker MARKER = MarkerFactory.getMarker("[KessokuEntityEvents]");
+}
