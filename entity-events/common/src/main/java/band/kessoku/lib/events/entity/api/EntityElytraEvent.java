4e466673b651ef2c6248fa5a9e055d5952613930
Oganesson897 <101081378+Oganesson897@users.noreply.github.com>
2024-09-02 22:14:06 +0900

    Spotless

--- entity-events/common/src/main/java/band/kessoku/lib/events/entity/api/EntityElytraEvent.java
+++ entity-events/common/src/main/java/band/kessoku/lib/events/entity/api/EntityElytraEvent.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright (c) 2024 KessokuTeaTime
+ *
+ * Licensed under the GNU Lesser General Pubic License, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package band.kessoku.lib.events.entity.api;
 
 import band.kessoku.lib.event.api.Event;
 import band.kessoku.lib.events.entity.api.item.KessokuElytraItem;
+
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
