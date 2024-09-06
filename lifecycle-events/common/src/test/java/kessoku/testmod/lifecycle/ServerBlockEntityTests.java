020a45f6bea70d1dd8708e1501d30301ac3d042f
TexBlock <3140846162@qq.com>
2024-09-06 14:35:44 +0800

    fix aj4j shadow issues

--- lifecycle-events/common/src/test/java/kessoku/testmod/lifecycle/ServerBlockEntityTests.java
+++ lifecycle-events/common/src/test/java/kessoku/testmod/lifecycle/ServerBlockEntityTests.java
@@ -1,12 +1,28 @@
+/*
+ * Copyright (c) 2024 KessokuTeaTime
+ *
+ * Licensed under the GNU Lesser General Pubic License, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package kessoku.testmod.lifecycle;
-
-import band.kessoku.lib.entrypoint.api.KessokuModInitializer;
-import band.kessoku.lib.events.lifecycle.api.ServerBlockEntityEvent;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.registry.Registries;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import band.kessoku.lib.entrypoint.api.KessokuModInitializer;
+import band.kessoku.lib.events.lifecycle.api.ServerBlockEntityEvent;
+
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.registry.Registries;
 
 public class ServerBlockEntityTests implements KessokuModInitializer {
     private final List<BlockEntity> serverBlockEntities = new ArrayList<>();
