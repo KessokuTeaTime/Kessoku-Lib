ed87cbe6686735f12d289c15023898b02ed8f058
TexBlock <3140846162@qq.com>
2024-09-06 19:36:08 +0800

    fix

--- buildSrc/src/main/java/band/kessoku/gradle/plugin/KessokuGradlePlugin.java
+++ buildSrc/src/main/java/band/kessoku/gradle/plugin/KessokuGradlePlugin.java
@@ -2,6 +2,7 @@ package band.kessoku.gradle.plugin;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.jetbrains.annotations.NotNull;
 
 public class KessokuGradlePlugin implements Plugin<Project> {
@@ -10,17 +11,29 @@ public class KessokuGradlePlugin implements Plugin<Project> {
     public void apply(@NotNull Project project) {
         project.getExtensions().create("kessoku", KesssokuExtension.class);
 
-        project.getRepositories().maven(repo -> {
+        additionalRepositories(project.getRepositories());
+    }
+
+    public void additionalRepositories(RepositoryHandler repositories) {
+        repositories.maven(repo -> {
             repo.setName("NeoForge");
             repo.setUrl("https://maven.neoforged.net/releases/");
         });
 
-        project.getRepositories().maven(repo -> {
+        repositories.maven(repo -> {
             repo.setName("AmarokIce's Maven");
             repo.setUrl("http://maven.snowlyicewolf.club/");
             repo.setAllowInsecureProtocol(true);
             repo.mavenContent(context -> {
-                context.includeGroup("club.someoneice.json");
+                context.includeGroup("club.someoneice");
+            });
+        });
+
+        repositories.maven(repo -> {
+            repo.setName("Jitpack Maven");
+            repo.setUrl("https://jitpack.io");
+            repo.mavenContent(context -> {
+                context.includeGroupByRegex("com\\.github\\..*");
             });
         });
     }
