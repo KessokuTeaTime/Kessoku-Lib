a2f344d2559c82b97cc3a96a81943443daac8b08
Chemiofitor4096 <hualongfentian123@outlook.com>
2024-10-02 21:44:46 +0800

    we don't need them in Data!!!

--- data/common/src/main/java/band/kessoku/lib/data/api/DataStructure.java
+++ data/common/src/main/java/band/kessoku/lib/data/api/DataStructure.java
@@ -6,7 +6,6 @@ package band.kessoku.lib.data.api;
  *     When building structure, data must be member variable and {@link DataStructure#integrate(Data) "integrate()"}
  *     can be used to hold data for other usages.
  * </p>
- * @see AbstractNBTStructure There also is an abstract structure for using.
  */
 public interface DataStructure {
     <T, K extends Data<T>> K integrate(K data);
