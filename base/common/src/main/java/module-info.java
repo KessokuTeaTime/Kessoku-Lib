96dbd47e9fc7cd55a0f4c1ba7a5a10c7aa291617
RawDiamondMC <RawDiamondMC@outlook.com>
2024-10-04 13:27:04 +0800

    So faq loom

--- base/common/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module kessoku.lib.base {
-    requires transitive org.jetbrains.annotations;
-    requires transitive org.slf4j;
-    exports band.kessoku.lib.api;
-    exports band.kessoku.lib.impl.base;
-    exports band.kessoku.lib.api.base;
-}
--- base/fabric/src/main/java/module-info.java
+++
@@ -1,4 +0,0 @@
-module kessoku.lib.base.fabric {
-    requires transitive kessoku.lib.base;
-    requires net.fabricmc.loader;
-}
