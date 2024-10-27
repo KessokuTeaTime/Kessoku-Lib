7b2d9b9fe5e9fcff825d7a5725948d5603e04be9
RawDiamondMC <RawDiamondMC@outlook.com>
2024-10-27 14:48:26 +0800

    fix: parameter name

--- base/common/src/main/java/band/kessoku/lib/api/base/reflect/ModifiersUtil.java
+++ base/common/src/main/java/band/kessoku/lib/api/base/reflect/ModifiersUtil.java
@@ -9,46 +9,46 @@ public final class ModifiersUtil {
 
     /* === Common === */
 
-    public static boolean isPublic(Member field) {
-        return Modifier.isPublic(field.getModifiers());
+    public static boolean isPublic(Member member) {
+        return Modifier.isPublic(member.getModifiers());
     }
 
-    public static boolean isStatic(Member field) {
-        return Modifier.isStatic(field.getModifiers());
+    public static boolean isStatic(Member member) {
+        return Modifier.isStatic(member.getModifiers());
     }
 
     /* === Common Combo === */
 
-    public static boolean isPublicAndStatic(Member field) {
-        return isPublic(field) && isStatic(field);
+    public static boolean isPublicAndStatic(Member member) {
+        return isPublic(member) && isStatic(member);
     }
 
-    public static boolean isPublicOrStatic(Member field, boolean shouldPublic, boolean shouldStatic) {
-        return shouldPublic == isPublic(field) && shouldStatic == isStatic(field);
+    public static boolean isPublicOrStatic(Member member, boolean shouldPublic, boolean shouldStatic) {
+        return shouldPublic == isPublic(member) && shouldStatic == isStatic(member);
     }
 
     /* === Common End === */
 
     /* === Data Object === */
 
-    public static boolean isVolatile(Member field) {
-        return Modifier.isVolatile(field.getModifiers());
+    public static boolean isVolatile(Member member) {
+        return Modifier.isVolatile(member.getModifiers());
     }
 
-    public static boolean isTransient(Member field) {
-        return Modifier.isTransient(field.getModifiers());
+    public static boolean isTransient(Member member) {
+        return Modifier.isTransient(member.getModifiers());
     }
 
     /* === Data Object End === */
 
     /* === Low Usage Tools === */
 
-    public static boolean isFinal(Member field) {
-        return Modifier.isFinal(field.getModifiers());
+    public static boolean isFinal(Member member) {
+        return Modifier.isFinal(member.getModifiers());
     }
 
-    public static boolean isProtected(Member field) {
-        return Modifier.isProtected(field.getModifiers());
+    public static boolean isProtected(Member member) {
+        return Modifier.isProtected(member.getModifiers());
     }
 
     /* === Low Usage Tools End === */
