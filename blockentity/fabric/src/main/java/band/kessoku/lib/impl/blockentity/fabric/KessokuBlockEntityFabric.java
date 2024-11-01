80157f770a5c9620561a4f15758703c97e6de435
Chemiofitor4096 <hualongfentian123@outlook.com>
2024-11-01 16:22:03 +0800

    KLib-BlockEntity...
    Fixed some grammatical errors in mod description.

---
+++ blockentity/fabric/src/main/java/band/kessoku/lib/impl/blockentity/fabric/KessokuBlockEntityFabric.java
@@ -0,0 +1,10 @@
+package band.kessoku.lib.impl.blockentity.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KessokuBlockEntityFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
